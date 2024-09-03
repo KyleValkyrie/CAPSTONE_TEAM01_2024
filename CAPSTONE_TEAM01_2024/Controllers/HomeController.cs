633b853ebf52082ca8f110669ae9f6a59cb00d0b
Kyle <baolegend9@gmail.com>
2024-09-03 22:33:24 +0700

    Initialization completed

---
+++ CAPSTONE_TEAM01_2024/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using CAPSTONE_TEAM01_2024.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CAPSTONE_TEAM01_2024.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["page"] = "Index";
+            return View();
+        }
+
+        public IActionResult HomePage()
+        {
+            ViewData["page"] = "HomePage";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

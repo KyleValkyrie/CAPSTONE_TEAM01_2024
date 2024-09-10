9209424e838d46ef179b980f672c639599220ebb
Kyle <baolegend9@gmail.com>
2024-09-10 12:55:39 +0700

    Update views/ Layout revision ver3.0/ Routing for modules

---
+++ CAPSTONE_TEAM01_2024/Controllers/ProfileManagerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CAPSTONE_TEAM01_2024.Controllers
+{
+    public class ProfileManagerController: Controller
+    {
+        public IActionResult ProfileManager()
+        {
+            ViewData["page"] = "ProfileManager";
+            return View();
+        }
+    }
+}

e43f53dfddb9488e7999ea83625e5628768180dd
Kyle <baolegend9@gmail.com>
2024-09-13 01:47:40 +0700

    Dropdown menu fix

---
+++ CAPSTONE_TEAM01_2024/Controllers/StatisticsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace CAPSTONE_TEAM01_2024.Controllers
+{
+    public class StatisticsController: Controller
+    {
+        public IActionResult ShowStatistics()
+        {
+            ViewData["page"] = "ShowStatistics";
+            return View();
+        }
+    }
+}

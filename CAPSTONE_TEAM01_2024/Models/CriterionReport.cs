7a30cb058d9c305d69173215cc759bec266c8a32
bin282001 <nam.197pm09478@vanlanguni.vn>
2024-11-22 19:08:04 +0700

    Done view, model Report

---
+++ CAPSTONE_TEAM01_2024/Models/CriterionReport.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace CAPSTONE_TEAM01_2024.Models
+{
+
+    public class CriterionReport
+    {
+        [Key]
+        public int CriterionId { get; set; }
+
+        [Required]
+        public string NameReport { get; set; }  // Criterion title
+
+        public string DescriptionReport { get; set; }  // Description for the criterion
+
+        // More fields could be added here as needed for criteria specifics
+
+    }
+}
---
+++ CAPSTONE_TEAM01_2024/Models/ReportDetail.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace CAPSTONE_TEAM01_2024.Models
+{
+
+    public class ReportDetail
+    {
+        [Key] 
+        public int DetailReportlId { get; set; }
+
+        [Required] 
+        public int ReportId { get; set; }
+        [JsonIgnore] 
+        public SemesterReport SemesterReport { get; set; }
+
+        [Required] 
+        public int CriterionId { get; set; }
+        public CriterionReport CriterionReport { get; set; }
+        
+        public string TaskReport { get; set; } // Task for each criterion
+        public string? HowToExecuteReport { get; set; } // Steps for execution
+        public List<AttachmentReport> AttachmentReport { get; set; } = new List<AttachmentReport>();
+        // Cá Nhân Tự Đánh Giá và Xếp Loại
+        public string SelfAssessment { get; set; } // Cá nhân tự đánh giá
+        public Ranking SelfRanking { get; set; } // Cá nhân tự xếp loại
+        
+        // Khai báo Enum trong Model
+        public enum Ranking
+        {
+            A,
+            B,
+            C,
+            D,
+            E
+        }
+
+        // BCN Khoa Đánh Giá và Xếp Loại
+        public string FacultyAssessment { get; set; } // BCN Khoa đánh giá
+        public Ranking FacultyRanking { get; set; } // BCN Khoa xếp loại
+    
+    }
+}
